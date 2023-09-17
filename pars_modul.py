378ea9d9e05b5cd1f98a30ba03c43f6e3379e5c1
KASSAS20 <kassas20@mail.ru>
2023-09-17 17:59:15 +0400

    fg

--- pars_modul.py
+++ pars_modul.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup as bs
 def pars_url_arists(soup):
     urls = soup.find_all('div')
     for i in urls:
-        print(urls, "12345678654")
+        print(urls, "12345678654")(dd)
