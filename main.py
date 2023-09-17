32e6dee23d2810d599f33caf05b8c0158a3867a3
KASSAS20 <kassas20@mail.ru>
2023-09-17 19:16:57 +0400

    add history

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pars_modul
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
-url = 'https://glamgo.store/'
-response = requests.get(url, headers=headers)
 
-soup = bs(response.text, 'lxml')
-
-
+soup = pars_modul.get_soup('https://glamgo.store/')
 dict_artist_url = pars_modul.pars_url_arists(soup)
 pars_modul.pars_url_product(dict_artist_url)
--- pars_modul.py
+++ pars_modul.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup as bs
+import requests
 
+def get_soup(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    soup = bs(response.text, 'lxml')
+    return soup
 
 def pars_url_arists(soup): #извлекает ссылки на артистов с главной страницы
     result = {}
@@ -13,5 +20,12 @@ def pars_url_arists(soup): #извлекает ссылки на артисто�
     return result
 
 def pars_url_product(dict_artist_url):
-    for i in dict_artist_url:
-        print(i)
+    for artist in dict_artist_url:
+        url = f'https://glamgo.store{dict_artist_url[artist]}'
+        soup = get_soup(url)
+        
+        d = soup.select(
+            '#rec237654106 > div.t776 > div > div.js-store-grid-cont.t-store__grid-cont.t-container.t-store__grid-cont_mobile-grid > div:nth-child(1) > a > div.t-store__card__textwrapper > div.js-store-prod-name.js-product-name.t-store__card__title.t-typography__title.t-name.t-name_md')
+        # print(d)
+        for i in d:
+            print(i)
