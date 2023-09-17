fb51132e9f00ff24846044d6ab0a5c8b9ff55656
KASSAS20 <kassas20@mail.ru>
2023-09-17 17:08:30 +0400

    unblock

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+url = 'https://glamgo.store/'
+response = requests.get(url, headers=headers)
+
+soup = bs(response.text, 'lxml')
+
+
+print(soup)
