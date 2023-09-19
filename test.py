2502757a81072529cf1d5c02297a592cb54b9276
KASSAS20 <kassas20@mail.ru>
2023-09-19 19:06:29 +0400

    test_dict

---
+++ test.py
@@ -0,0 +1,2 @@
+# {'CAKEBOY': {'ФУТБОЛКА «SMILES»': 'https://static.tildacdn.com/tild3932-6164-4736-a538-363338666433/4.jpg', 'ФУТБОЛКА «HAIRSTYLE»': 'https://static.tildacdn.com/tild3236-6633-4737-b438-396265663062/photo.jpg', 'ХУДИ «ОКОЛЬНЫЙ ПУТЬ НА ЗАПАД»':
+#              'https://static.tildacdn.com/tild3938-3366-4136-a436-343436313163/OP4i35tbaJo.jpg'}, 'IROH': {'ФУТБОЛКА «XANNY»': 'https://static.tildacdn.com/tild3235-3539-4632-a539-643238623233/photo_2020-10-28_00-.jpg'}, 'Flipper Floyd': {None: None}}
